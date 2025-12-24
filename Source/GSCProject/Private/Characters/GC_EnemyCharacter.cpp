d942d00d91ba22de88fa85a593441d22aafd18b1
Bikarjx <Bikarjx@gmail.com>
2025-12-24 20:41:36 +0700

    Add enemy character class and assets
    Introduced AGC_EnemyCharacter C++ class with ability system integration. Added melee and ranged enemy blueprints and animation assets. Moved player character blueprint to Player folder. Updated game mode and debug map to support new enemy characters.

---
+++ Source/GSCProject/Private/Characters/GC_EnemyCharacter.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright Bika Ridjalluddin
+
+
+#include "Characters/GC_EnemyCharacter.h"
+
+#include "AbilitySystemComponent.h"
+
+
+// Sets default values
+AGC_EnemyCharacter::AGC_EnemyCharacter()
+{
+
+	PrimaryActorTick.bCanEverTick = false;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+}
+
+UAbilitySystemComponent* AGC_EnemyCharacter::GetAbilitySystemComponent() const
+{
+	return Super::GetAbilitySystemComponent();
+}
+
+
+void AGC_EnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (!IsValid(GetAbilitySystemComponent())) return;
+	
+	GetAbilitySystemComponent()->InitAbilityActorInfo(this,this);
+	
+	if (!HasAuthority()) return;
+	
+	GiveStartupAbilities();
+}
+
---
+++ Source/GSCProject/Public/Characters/GC_EnemyCharacter.h
@@ -0,0 +1,28 @@
+﻿// Copyright Bika Ridjalluddin
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GC_BaseCharacter.h"
+#include "GC_EnemyCharacter.generated.h"
+class UAbilitySystemComponent;
+
+UCLASS()
+class GSCPROJECT_API AGC_EnemyCharacter : public AGC_BaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	AGC_EnemyCharacter();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+};
