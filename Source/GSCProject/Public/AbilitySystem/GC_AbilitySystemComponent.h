600784485e6ff639a2209c5e7fbd8291c59f303d
Bikarjx <Bikarjx@gmail.com>
2025-12-26 16:51:09 +0700

    Add ability level management functions to AbilitySystemComponent
    Introduced SetAbilityLevel and AddToAbilityLevel methods to GC_AbilitySystemComponent, allowing ability levels to be set or incremented via Blueprint. Updated input and ability assets to support a new level-up ability action, and added a function redirect in DefaultEngine.ini for backward compatibility.

--- Source/GSCProject/Public/AbilitySystem/GC_AbilitySystemComponent.h
+++ Source/GSCProject/Public/AbilitySystem/GC_AbilitySystemComponent.h
@@ -16,6 +16,12 @@ public:
 	virtual void OnGiveAbility(FGameplayAbilitySpec& AbilitySpec) override;
 	virtual void OnRep_ActivateAbilities() override;
 	
+	UFUNCTION(BlueprintCallable, Category="GSCP|Abilities")
+	void SetAbilityLevel(TSubclassOf<UGameplayAbility> AbilityClass, int32 Level);
+	
+	UFUNCTION(BlueprintCallable, Category="GSCP|Abilities")
+	void AddToAbilityLevel(TSubclassOf<UGameplayAbility> AbilityClass, int32 Level = 1);
+	
 private:
 	
 	void HandleAutoActivateAbilities(const FGameplayAbilitySpec& AbilitySpec);
